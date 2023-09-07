bbc795faa35a0bc81b4d87330aae7c87a5de5c89
sathwik1361 <sathwikameenabad@gmail.com>
2023-09-07 20:23:15 +0530

    sec com

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -3,7 +3,8 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//this is the main class
+//this is the main class class
+
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
